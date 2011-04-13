660dfc7810153a7f7fe4307c202808a840adc472
Guido Tapia <guido.tapia@picnet.com.au>
2011-04-13 12:22:24 +1000

    Pre cleancode test commit

--- update_links.bat
+++ update_links.bat
@@ -1,7 +1,7 @@
-set CLOSURE_LIB_SOURCE_DIR=c:\dev\Projects\Misc\picnet-closure-library-fork\closure_library
+set CLOSURE_LIB_SOURCE_DIR=c:\dev\Projects\Misc\picnet-closure-library-fork\closure-library
 set CLOSURE_COMPILER_SOURCE_DIR=C:\dev\libs\closure-compiler\build\compiler.jar
 
-set CLOSURE_LIB_TARGET_DIR=lib\closure_library
+set CLOSURE_LIB_TARGET_DIR=lib\closure-library
 set CLOSURE_COMPILER_TARGET_DIR=lib\compiler.jar
 
 rmdir %CLOSURE_LIB_TARGET_DIR%
