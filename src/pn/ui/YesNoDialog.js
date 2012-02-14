1874bb3fcd54f2115e22a3fbbe43c4aa085b3aaa
Guido Tapia <guido@tapia.com.au>
2012-02-14 14:04:19 +1100

    Added a simple reusable yes no dialog

---
+++ src/pn/ui/YesNoDialog.js
@@ -0,0 +1,49 @@
+
+goog.provide('pn.ui.YesNoDialog');
+
+goog.require('goog.ui.Dialog');
+goog.require('goog.ui.Dialog.ButtonSet');
+goog.require('goog.ui.Dialog.EventType');
+
+
+/**
+ * @constructor
+ * @param {!string} title The title of this dialog
+ * @param {!string} content The content of this dialog
+ */
+pn.ui.YesNoDialog = function(title, content) {
+
+  /**
+   * @private
+   * @type {!string}
+   */
+  this.title_ = title;
+
+  /**
+   * @private
+   * @type {!string}
+   */
+  this.content_ = content;  
+};
+
+
+/**
+ * @param {function(boolean):undefined} callback The callback 
+ */
+pn.ui.YesNoDialog.prototype.show = function(callback) {
+  goog.asserts.assert(callback);
+
+  var dialog = new goog.ui.Dialog();
+  dialog.setContent(this.content_);
+  dialog.setTitle(this.title_);
+  dialog.setModal(true);
+  dialog.setButtonSet(goog.ui.Dialog.ButtonSet.createYesNoCancel());
+
+  goog.events.listen(dialog, goog.ui.Dialog.EventType.SELECT, function(e) {
+    dialog.dispose();
+    if (e.key === 'cancel') { return; }
+    callback(e.key === 'yes');
+  }, false, this);
+
+  dialog.setVisible(true);
+};
