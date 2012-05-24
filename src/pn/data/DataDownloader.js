1659356e2f087627d1ebb2b06cf10b306b59843c
Guido Tapia <guido@tapia.com.au>
2012-05-25 07:45:55 +1000

    Fixed large memory leak with exporting files

--- src/pn/data/DataDownloader.js
+++ src/pn/data/DataDownloader.js
@@ -1,6 +1,7 @@
 ﻿;
 goog.provide('pn.data.DataDownloader');
 
+goog.require('goog.net.EventType');
 goog.require('goog.net.IframeIo');
 
 
@@ -15,19 +16,21 @@ pn.data.DataDownloader.send = function(url, data, opt_cb, opt_timeout) {
   goog.asserts.assert(url);
   // NOTE: Callback will not be called if we are downloading a file.
   var cb = function(e) {
-    if (opt_cb) opt_cb(e && e.target ? e.target.getResponseHtml() : null);
+    if (opt_cb) { opt_cb(e && e.target ? e.target.getResponseHtml() : null); }
     opt_cb = undefined;
   };
+
+  var io = new goog.net.IframeIo();
+  goog.events.listen(io, goog.net.EventType.READY, io.dispose, false, io);
+  goog.events.listen(io, goog.net.EventType.COMPLETE, cb);
+  io.send(url, 'POST', true, data);
 
   // This hack is required as downloading files with IframeIo does not
   // work (response is ended and the internal form is not disposed).
   // See report:
   /** @suppress {accessControls} */
   goog.Timer.callOnce(function() {
-    goog.dispose(goog.net.IframeIo.form_);
-    delete goog.net.IframeIo.form_;
+    goog.dispose(io);
     cb(null);
   }, opt_timeout || 2000);
-
-  goog.net.IframeIo.send(url, cb, 'POST', true, data);
 };
