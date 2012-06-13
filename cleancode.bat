d9e990ad3a42c969e4fc2b83825e906fb85b3fa0
Guido Tapia <guido@tapia.com.au>
2012-06-13 16:30:03 +1000

    Added TemplateWrapper control which allows a Grid or Edit component to be wrapped in a soy template

--- cleancode.bat
+++ cleancode.bat
@@ -1,7 +1,6 @@
 set ARGS=--strict -r src\ ^
 -e tests,demos ^
--x src\deps.js,src\pn\slick.grid.externs.js ^
--x src\deps.js,src\pn\rx.externs.js
+-x src\deps.js,src\pn\slick.grid.externs.js,src\pn\rx.externs.js
 
 fixjsstyle %ARGS%
 gjslint %ARGS%
---
+++ src/pn/ui/TemplateWrapper.js
@@ -0,0 +1,64 @@
+﻿;
+goog.provide('pn.ui.TemplateWrapper');
+
+goog.require('goog.ui.Component');
+
+
+
+/**
+ * @constructor
+ * @extends {goog.ui.Component}
+ * @param {string} html The template html to display.
+ * @param {string} idOfParent The id of the parent DOM element in the specified
+ *    html to attach the component to.
+ * @param {!goog.ui.Component} component The component to attach to the
+ *    specified html.
+ */
+pn.ui.TemplateWrapper = function(html, idOfParent, component) {
+  goog.asserts.assert(html);
+  goog.asserts.assert(idOfParent);
+  goog.asserts.assert(component);
+
+  goog.ui.Component.call(this);
+
+  /**
+   * @private
+   * @const
+   * @type {string}
+   */
+  this.html_ = html;
+
+  /**
+   * @private
+   * @const
+   * @type {string}
+   */
+  this.idOfParent_ = idOfParent;
+
+  /**
+   * @private
+   * @const
+   * @type {!goog.ui.Component}
+   */
+  this.component_ = component;
+  this.registerDisposable(component);
+};
+goog.inherits(pn.ui.TemplateWrapper, goog.ui.Component);
+
+
+/** @inheritDoc */
+pn.ui.TemplateWrapper.prototype.createDom = function() {
+  this.decorateInternal(this.dom_.createElement('div'));
+};
+
+
+/** @inheritDoc */
+pn.ui.TemplateWrapper.prototype.decorateInternal = function(element) {
+  this.setElementInternal(element);
+
+  var node = /** @type {!Element} */ (
+      goog.dom.htmlToDocumentFragment(this.html_));
+  goog.dom.appendChild(element, node);
+  var parent = pn.dom.getElement(this.idOfParent_);
+  this.component_.decorate(parent);
+};
