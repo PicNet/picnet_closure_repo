33346e39f833e5dbf40244c574dd5ec8d0088e85
Guido Tapia <guido@tapia.com.au>
2012-06-19 14:23:31 +1000

    Added a intRenderer to the FieldRenderers

--- src/pn/ui/edit/FieldRenderers.js
+++ src/pn/ui/edit/FieldRenderers.js
@@ -74,7 +74,25 @@ pn.ui.edit.FieldRenderers.centsRenderer = function(field) {
     return pn.convert.currencyToCents(input.value);
   };
   return input;
+};
 
+
+/**
+ * @param {!pn.ui.FieldCtx} field The field to render.
+ * @return {!Element} The int input control.
+ */
+pn.ui.edit.FieldRenderers.intRenderer = function(field) {
+  var val = field.getEntityValue() || 0;
+  var input = goog.dom.createDom('input', {
+    'class': 'int-field',
+    'type': 'number'
+  });
+  input.value = val.toString();
+  goog.dom.appendChild(field.parentComponent, input);
+  input.getValue = function() {
+    return parseInt(input.value, 10) || 0;
+  };
+  return input;
 };
 
 
