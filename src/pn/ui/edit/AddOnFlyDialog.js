d9fb40e76675f058845579a426f41af768255fa9
Guido Tapia <guido@tapia.com.au>
2012-07-09 14:03:22 +1000

    Added missing requires

--- src/pn/ui/edit/AddOnFlyDialog.js
+++ src/pn/ui/edit/AddOnFlyDialog.js
@@ -3,6 +3,7 @@ goog.provide('pn.ui.edit.AddOnFlyDialog');
 goog.provide('pn.ui.edit.AddOnFlyDialog.EventType');
 
 goog.require('goog.events.Event');
+goog.require('goog.events.EventHandler');
 goog.require('goog.events.EventTarget');
 goog.require('goog.ui.Dialog');
 goog.require('pn.ui.Dialog');
