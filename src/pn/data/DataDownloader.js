d8225036212ed0080011ece825c2208b41bf79d4
Guido Tapia <guido@tapia.com.au>
2012-07-25 08:26:17 +1000

    DataDownloader now disposes its internal iframe

--- src/pn/data/DataDownloader.js
+++ src/pn/data/DataDownloader.js
@@ -3,21 +3,37 @@ goog.provide('pn.data.DataDownloader');
 
 goog.require('goog.net.IframeIo');
 
+/** 
+ * @private
+ * @type {goog.net.IframeIo}
+ */
+pn.data.DataDownloader.io_ = null;
 
 /**
  * @param {string} url The export http GET url.
  * @param {Object} data The data to send to the http request.
  * @param {function(string):undefined=} opt_cb The oncomplete callback with
  *    the response html as the argument.
- * @param {number=} opt_timeout An optional timeout for the request.
  */
-pn.data.DataDownloader.send = function(url, data, opt_cb, opt_timeout) {
+pn.data.DataDownloader.send = function(url, data, opt_cb) {
   goog.asserts.assert(url);
+  var dispose = function() {
+    if (!pn.data.DataDownloader.io_) return;
+    pn.data.DataDownloader.io_.dispose();
+    pn.data.DataDownloader.io_ = null;
+  };
+  dispose();
+
   // NOTE: Callback will not be called if we are downloading a file.
   var cb = function(e) {
+    dispose();
     if (opt_cb) opt_cb(e && e.target ? e.target.getResponseHtml() : null);
     opt_cb = undefined;
   };
-  if (opt_cb && opt_timeout) { goog.Timer.callOnce(cb, opt_timeout); }
-  goog.net.IframeIo.send(url, cb, 'POST', true, data);
+
+  pn.data.DataDownloader.io_ = new goog.net.IframeIo();
+  goog.events.listenOnce(
+      pn.data.DataDownloader.io_, goog.net.EventType.COMPLETE, cb);
+  pn.data.DataDownloader.io_.send(url, 'POST', true, data);
+  goog.Timer.callOnce(dispose, 20000);  
 };
