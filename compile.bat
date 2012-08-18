1fadd90bd543ef25c3dca53d9003bf25dd9f25b7
Guido Tapia <guido@tapia.com.au>
2012-08-18 11:25:03 +1000

    Created a vs2012 solution for the project

--- compile.bat
+++ compile.bat
@@ -17,12 +17,11 @@ c:\Python27\python.exe ^
   --output_mode=compiled ^
   --compiler_jar=U:\shared\lib\picnetcompiler.jar ^
   --compiler_flags="--externs=src\pn\rx.externs.js" ^
-  --compiler_flags="--externs=src\pn\slick.grid.externs.js" ^
-  --compiler_flags="--externs=U:\shared\lib\closure-compiler-src\contrib\externs\jquery-1.6.js" ^
+  --compiler_flags="--externs=src\pn\slick.grid.externs.js" ^  
   --compiler_flags="--debug=true" ^
   --compiler_flags="--process_closure_primitives=true" ^
   --compiler_flags="--warning_level=VERBOSE" ^
-    --compiler_flags="--jscomp_warning=accessControls" ^
+  --compiler_flags="--jscomp_warning=accessControls" ^
   --compiler_flags="--jscomp_warning=checkRegExp" ^
   --compiler_flags="--jscomp_warning=checkTypes" ^
   --compiler_flags="--jscomp_warning=checkVars" ^
