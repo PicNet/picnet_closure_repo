df63cf33efe522f47a1a1f1dbb23163efd694e80
Guido Tapia <guido@tapia.com.au>
2012-08-31 11:01:56 +1000

    Fixed bugs in DelayedThrottleInputListener.js

--- src/pn/ui/DelayedThrottleInputListener.js
+++ src/pn/ui/DelayedThrottleInputListener.js
@@ -1,28 +1,30 @@
-﻿
+﻿;
 goog.provide('pn.ui.DelayedThrottleInputListener');
 
-goog.require('goog.events.EventTarget');
-goog.require('goog.events.EventHandler');
-goog.require('goog.events.Event');
-goog.require('goog.events.EventType');
 goog.require('goog.Timer');
+goog.require('goog.events.Event');
+goog.require('goog.events.EventHandler');
+goog.require('goog.events.EventTarget');
+goog.require('goog.events.EventType');
+
+
 
 /**
  * @constructor
- * @param {!Element} input The control to listen to
+ * @param {!Element} input The control to listen to.
  * @param {number} delay The delay to wait for further updates before raising
  *    a notification.
- * @param {string} opt_eventType The optional event type to listen to, if 
- *    this is not specified then a smart event type is inferred
+ * @param {string=} opt_eventType The optional event type to listen to, if
+ *    this is not specified then a smart event type is inferred.
  * @extends {goog.events.EventTarget}
  */
 pn.ui.DelayedThrottleInputListener = function(input, delay, opt_eventType) {
   goog.asserts.assert(input);
   goog.asserts.assert(goog.isNumber(delay) && delay > 0);
-  goog.asserts.assert(!goog.isDef(opt_eventType) || 
+  goog.asserts.assert(!goog.isDef(opt_eventType) ||
       goog.isString(opt_eventType));
 
-  goog.events.EventTarget.call(this);  
+  goog.events.EventTarget.call(this);
 
   /**
    * @private
@@ -44,6 +46,12 @@ pn.ui.DelayedThrottleInputListener = function(input, delay, opt_eventType) {
 
   /**
    * @private
+   * @type {string}
+   */
+  this.lastFilterValue_ = '';
+
+  /**
+   * @private
    * @type {number}
    */
   this.lastInputTime_ = 0;
@@ -52,7 +60,7 @@ pn.ui.DelayedThrottleInputListener = function(input, delay, opt_eventType) {
    * @private
    * @type {number}
    */
-  this.timerId_ = 0;  
+  this.timerId_ = 0;
 
   /**
    * @private
@@ -72,43 +80,55 @@ goog.inherits(pn.ui.DelayedThrottleInputListener, goog.events.EventTarget);
  *    input control used as the source.
  */
 pn.ui.DelayedThrottleInputListener.prototype.inferEventType_ = function() {
-  return this.input_.getAttribute('type') === 'text' ? 
+  return this.input_.getAttribute('type') === 'text' ?
       goog.events.EventType.KEYUP :
       goog.events.EventType.CHANGE;
 };
+
 
 /** @private */
 pn.ui.DelayedThrottleInputListener.prototype.init_ = function() {
   this.eh_.listen(this.input_, this.eventType_, this.onInputEvent_);
 };
 
+
 /** @private */
 pn.ui.DelayedThrottleInputListener.prototype.onInputEvent_ = function() {
   this.lastInputTime_ = new Date().getTime();
+
+  if (this.timerId_) {
+    goog.Timer.clear(this.timerId_);
+    this.timerId_ = 0;
+  }
   this.checkTimer_();
 };
 
+
 /** @private */
 pn.ui.DelayedThrottleInputListener.prototype.checkTimer_ = function() {
-  if (this.timerId_) {
-    clearTimeout(this.timerId_);
-    this.timerId_ = 0;
-  }
-
   var curtime = new Date().getTime();
-  if (curtime - this.lastInputTime_ >= this.delay) { this.fireChanged_(); } 
-  else { 
+  if (curtime - this.lastInputTime_ >= this.delay_) {
+    this.fireIfChanged_();
+    this.timerId_ = 0;
+  } else {
     var del = this.delay_ / 3;
-    this.timerId_ = goog.Timer.callOnce(this.checkTimer_, del, this); 
+    this.timerId_ = goog.Timer.callOnce(this.checkTimer_, del, this);
   }
 };
 
-/** private */
-pn.ui.DelayedThrottleInputListener.prototype.fireChanged_ = function() {  
+
+/** @private */
+pn.ui.DelayedThrottleInputListener.prototype.fireIfChanged_ = function() {
   clearTimeout(this.timerId_);
-  var eventType = pn.ui.DelayedThrottleInputListener.CHANGED;
-  this.dispatchEvent(new goog.events.Event(eventType));
+  var value = this.input_.value;
+
+  if (this.lastFilterValue_ !== value) {
+    var e = new goog.events.Event(pn.ui.DelayedThrottleInputListener.CHANGED);
+    e.value = this.lastFilterValue_ = value;
+    this.dispatchEvent(e);
+  }
 };
+
 
 /**
  * @const
