93d71eed6529afa5068c30b691ca12c738260fed
Guido Tapia <guido@tapia.com.au>
2012-10-20 09:45:09 +1100

    New model and collections branched based on Angular/Backbone style models

--- src/pn/model/Model.js
+++ src/pn/model/Model.js
@@ -2,73 +2,58 @@
 goog.provide('pn.model.Model');
 
 goog.require('pn.model.ModelBase');
-goog.require('pn.model.TimerInstance');
 
 
 
 /**
  * @constructor
  * @extends {pn.model.ModelBase}
- * @param {!Object} src The source object to create the model from.
- * @param {boolean=} opt_register Wether to register this model in the timed
- *    check (default = true).
  */
-pn.model.Model = function(src, opt_register) {
-  pn.assObj(src);
-
+pn.model.Model = function() {
   pn.model.ModelBase.call(this);
 
   /**
    * @private
-   * @type {!Object}
+   * @type {!Object.<*>}
    */
-  this.src_ = src;
-
-  /**
-   * @private
-   * @type {boolean}
-   */
-  this.register_ = opt_register !== false;
-
-  /**
-   * @private
-   * @type {!Object}
-   */
-  this.last_ = {};
-
-  for (var i in src) {
-    var fld = src[i];
-    var val = fld && goog.isFunction(fld.clone) ? fld.clone() : fld;
-    this[i] = this.last_[i] = val;
-  }
-
-  if (this.register_) { pn.model.TimerInstance.register(this); }
+  this.values_ = {};  
 };
 goog.inherits(pn.model.Model, pn.model.ModelBase);
 
-
-/** @override */
-pn.model.Model.prototype.getChanges = function() {
-  var changes = [];
-  for (var i in this.src_) {
-    if (!this.areSame(this.src_[i], this.last_[i])) {
-      /** @type {pn.model.Model.Change} */
-      var change = {field: i, oldval: this.last_[i], newval: this.src_[i]};
-      changes.push(change);
-      this.last_[i] = this.src_[i];
-    }
-  }
-  return changes;
+pn.model.Model.prototype.get = function(name) {
+  return this.values_[name];
 };
 
-
-/** @override */
-pn.model.Model.prototype.disposeInternal = function() {
-  pn.model.Model.superClass_.disposeInternal.call(this);
-
-  if (this.register_) { pn.model.TimerInstance.deregister(this); }
+/**
+ * @param {string} name The name of the property to set.
+ * @param {*} val The new value of the property to set.
+ */
+pn.model.Model.prototype.set = function(name, val) {
+  var old = this.values_[name];
+  if (this.same(old, val)) return;  
+  this.queueChange(this, name, old, val);
 };
 
+/**
+ * @constructor
+ * @param {!pn.model.ModelBase} model The model that changed.
+ * @param {string} property The name of the property that changed.
+ * @param {*} oldv The old value of the changed property.
+ * @param {*} newv The new value of the changed property.
+ */
+pn.model.Change = function(model, property, oldv, newv) {
+  pn.assInst(model, pn.model.ModelBase);
+  pn.assStr(property);
 
-/** @typedef {{field:string, oldval: *, newval: *}} */
-pn.model.Model.Change;
+  /** @type {!pn.model.ModelBase} */
+  this.model = model;
+
+  /** @type {string} */
+  this.property = property;
+
+  /** @type {*} */
+  this.oldv = oldv;
+
+  /** @type {*} */
+  this.newv = newv;
+}; 
--- src/pn/model/ModelBase.js
+++ src/pn/model/ModelBase.js
@@ -1,30 +1,98 @@
 
 goog.provide('pn.model.ModelBase');
 
+goog.require('pn');
 goog.require('goog.events.EventTarget');
 goog.require('pn.model.ChangeEvent');
-goog.require('pn.model.TimerInstance');
-
+goog.require('goog.async.Delay');
 
 
 /**
  * @constructor
  * @extends {goog.events.EventTarget}
  */
-pn.model.ModelBase = function() {};
-goog.inherits(pn.model.ModelBase, goog.events.EventTarget);
+pn.model.ModelBase = function() {
 
+  /** 
+   * @private
+   * @type {goog.async.Delay}
+   */
+  this.delay_ = new goog.async.Delay(this.fire_.pnbind(this), 2);
+
+  /** 
+   * @private
+   * @type {!Array.<!pn.model.Change>}
+   */
+  this.changes_ = [];
+};
+goog.inherits(pn.model.ModelBase, goog.events.EventTarget);
 
 /** @return {!Array} The changes since last time getChanges were called. */
 pn.model.ModelBase.prototype.getChanges = goog.abstractMethod;
-
 
 /**
  * @param {*} a The first item to compare.
  * @param {*} b The second item to compare.
  * @return {boolean} Wether a and b are equivalent.
  */
-pn.model.ModelBase.prototype.areSame = function(a, b) {
+pn.model.ModelBase.prototype.same = function(a, b) {
   if (!goog.isDefAndNotNull(a)) return !goog.isDefAndNotNull(b);
   return goog.isFunction(a.equals) ? a.equals(b) : a === b;
+};
+
+/**
+ * @protected
+ * @param {string} property The name of the property that changed.
+ * @param {*} oldv The old value of the changed property.
+ * @param {*} newv The new value of the changed property.
+ */
+pn.model.ModelBase.prototype.queueChange = function(prop, oldv, newv) {
+  pn.assStr(prop);
+
+  this.changes_.push(new pn.model.Change(this, prop, oldv, newv));
+  this.startTimer_();
+};
+
+pn.model.ModelBase.prototype.startTimer_ = function() {
+  this.delay_.start();
+};
+
+pn.model.ModelBase.prototype.fire_ = function() {
+  var changes = this.changes_;
+  this.changes_ = [];
+
+  this.dispatchEvent(new pn.model.ChangeEvent(changes));
+};
+
+/**
+ * @constructor
+ * @param {!pn.model.ModelBase} model The model that changed.
+ * @param {string} property The name of the property that changed.
+ * @param {*} oldv The old value of the changed property.
+ * @param {*} newv The new value of the changed property.
+ */
+pn.model.Change = function(model, property, oldv, newv) {
+  pn.assInst(model, pn.model.ModelBase);
+  pn.assStr(property);
+
+  /** @type {!pn.model.ModelBase} */
+  this.model = model;
+
+  /** @type {string} */
+  this.property = property;
+
+  /** @type {*} */
+  this.oldv = oldv;
+
+  /** @type {*} */
+  this.newv = newv;
+}; 
+
+pn.model.Change.prototype.equals = function(other) {
+  if (!other || !(other instanceof pn.model.Change)) return false;
+  if (other === this) return true;  
+  return other.model === this.model &&
+    other.property == this.property &&
+    other.oldv == this.oldv &&
+    other.newv == this.newv;
 };
