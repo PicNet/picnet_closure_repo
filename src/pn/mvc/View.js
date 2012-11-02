bedeb878e6fd2aa4ffa10be6e0e49cdaba0116dc
Guido Tapia <guido@tapia.com.au>
2012-11-02 11:38:12 +1100

    Made the model optional for simple views

--- src/pn/mvc/View.js
+++ src/pn/mvc/View.js
@@ -10,11 +10,11 @@ goog.require('pn.mvc.EventType');
  * @constructor
  * @extends {pn.app.EventHandlerTarget}
  * @param {!Element} el The DOM element to attach this View to.
- * @param {!pn.mvc.ModelBase} model The model for this view.
+ * @param {pn.mvc.ModelBase=} opt_model The optional model for this view.
  */
-pn.mvc.View = function(el, model) {
+pn.mvc.View = function(el, opt_model) {
   pn.assInst(el, HTMLElement);
-  pn.assInst(model, pn.mvc.ModelBase);
+  pn.ass(!opt_model || opt_model instanceof pn.mvc.ModelBase);
 
   pn.app.EventHandlerTarget.call(this);
 
@@ -26,18 +26,19 @@ pn.mvc.View = function(el, model) {
 
   /**
    * @private
-   * @type {!pn.mvc.ModelBase}
+   * @type {pn.mvc.ModelBase}
    */
-  this.model_ = model;
-  this.registerDisposable(this.model_);
-  this.listen(this.model_, pn.mvc.EventType.CHANGE, this.refresh);
-
+  this.model_ = opt_model || null;
+  if (this.model_) {
+    this.registerDisposable(this.model_);
+    this.listen(this.model_, pn.mvc.EventType.CHANGE, this.refresh);
+  }
   this.decorate(this.el_);
 };
 goog.inherits(pn.mvc.View, pn.app.EventHandlerTarget);
 
 
-/** @return {!pn.mvc.ModelBase} The view's model. */
+/** @return {pn.mvc.ModelBase} The view's model. */
 pn.mvc.View.prototype.getModel = function() { return this.model_; };
 
 
@@ -50,6 +51,6 @@ pn.mvc.View.prototype.decorate = goog.abstractMethod;
 
 /**
  * @protected
- * @param {!pn.mvc.ChangeEvent} e The model change event.
+ * @param {pn.mvc.ChangeEvent} e The model change event.
  */
 pn.mvc.View.prototype.refresh = goog.abstractMethod;
