782e065b302aafeb79979fd78157f858b15fdb04
Guido Tapia <guido@tapia.com.au>
2012-11-05 10:14:47 +1100

    Released the table filter

--- src/pn/app/BaseApp.js
+++ src/pn/app/BaseApp.js
@@ -1,8 +1,8 @@
 
 goog.require('goog.Disposable');
 goog.require('goog.debug.Logger');
-goog.require('goog.pubsub.PubSub');
 goog.require('goog.events.EventHandler');
+goog.require('goog.pubsub.PubSub');
 goog.require('pn');
 goog.require('pn.app.AppConfig');
 goog.require('pn.app.EventBus');
