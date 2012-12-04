91e64265612584d07cac78cbb47175eb19e56cf2
Guido Tapia <guido@tapia.com.au>
2012-12-05 08:55:23 +1100

    grid.Command now supports passing in custom made element

--- src/pn/ui/grid/cmd/Command.js
+++ src/pn/ui/grid/cmd/Command.js
@@ -1,6 +1,7 @@
 ﻿;
 goog.provide('pn.ui.grid.cmd.Command');
 
+goog.require('goog.Disposable');
 goog.require('goog.events.Event');
 goog.require('goog.events.EventHandler');
 goog.require('goog.ui.Button');
@@ -16,8 +17,11 @@ goog.require('pn.ui.grid.cmd.ICommand');
  * @param {string} name The name/caption of this column.
  * @param {string} eventType The event to fire on the component action.
  * @param {string=} opt_tooltip The optional tooltip for this command.
+ * @param {(Element|goog.ui.Component)=} opt_elem The optional element for
+ *    this command.  If specified no additional event work is done on
+ *    this command.
  */
-pn.ui.grid.cmd.Command = function(name, eventType, opt_tooltip) {
+pn.ui.grid.cmd.Command = function(name, eventType, opt_tooltip, opt_elem) {
   pn.ass(name);
   pn.ass(eventType);
 
@@ -52,9 +56,15 @@ pn.ui.grid.cmd.Command = function(name, eventType, opt_tooltip) {
 
   /**
    * @private
-   * @type {goog.ui.Button}
+   * @type {boolean}
    */
-  this.commandElement_ = null;
+  this.hasExplicitElem_ = !!opt_elem;
+
+  /**
+   * @private
+   * @type {Element|goog.ui.Component}
+   */
+  this.commandElement_ = opt_elem || null;
 };
 goog.inherits(pn.ui.grid.cmd.Command, goog.ui.Component);
 
@@ -68,18 +78,27 @@ pn.ui.grid.cmd.Command.prototype.createDom = function() {
 /** @override */
 pn.ui.grid.cmd.Command.prototype.decorateInternal = function(element) {
   this.setElementInternal(element);
-  this.commandElement_ = new goog.ui.Button(this.name_);
-  this.registerDisposable(this.commandElement_);
+  if (!this.hasExplicitElem_) {
+    this.commandElement_ = new goog.ui.Button(this.name_);
+    this.commandElement_.setTooltip(this.tooltip_);
+    this.commandElement_.enableClassName(
+        goog.string.removeAll(this.name_.toLowerCase(), ''), true);
+  }
+  if (this.commandElement_ instanceof goog.Disposable) {
+    this.registerDisposable(/** @type {goog.Disposable} */ (
+        this.commandElement_));
+  }
 
-  this.commandElement_.setTooltip(this.tooltip_);
-  this.commandElement_.enableClassName(
-      goog.string.removeAll(this.name_.toLowerCase(), ''), true);
-  this.commandElement_.render(element);
+  if (this.commandElement_.render) this.commandElement_.render(element);
+  else goog.dom.appendChild(element, /** @type {!Element} */ (
+      this.commandElement_));
 };
 
 
 /** @override */
 pn.ui.grid.cmd.Command.prototype.enterDocument = function() {
+  if (this.hasExplicitElem_) return;
+
   var action = goog.ui.Component.EventType.ACTION;
   this.getHandler().listen(this.commandElement_, action, function() {
     this.dispatchEvent(new goog.events.Event(this.eventType, this));
