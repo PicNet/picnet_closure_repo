7d781ff000127d16142415e050a9c6293d85709f
Guido Tapia <guido@tapia.com.au>
2013-02-14 14:01:45 +1100

    Minor refactoring of FileUpload

--- src/pn/ui/FileUpload.js
+++ src/pn/ui/FileUpload.js
@@ -75,13 +75,6 @@ pn.ui.FileUpload = function(id, serverAction, opt_getData, opt_validateData) {
 goog.inherits(pn.ui.FileUpload, goog.ui.Component);
 
 
-/**
- * @type {string}
- * @const
- */
-pn.ui.FileUpload.UPLOAD_START = 'upload-start';
-
-
 /** @override */
 pn.ui.FileUpload.prototype.createDom = function() {
   this.decorateInternal(this.dom_.createElement('div'));
@@ -119,7 +112,7 @@ pn.ui.FileUpload.prototype.doUpload_ = function() {
   if (this.validateData_ &&
       !this.validateData_.call(this, this.fileInput_.value)) return;
 
-  var e = new goog.events.Event(pn.ui.FileUpload.UPLOAD_START, this);
+  var e = new goog.events.Event(pn.ui.FileUpload.EventType.UPLOAD_START, this);
   this.dispatchEvent(e);
 
   this.io_ = new goog.net.IframeIo();
@@ -182,10 +175,9 @@ pn.ui.FileUpload.prototype.onComplete_ = function() {
 };
 
 
-/**
- * @enum {string}
- */
+/** @enum {string} */
 pn.ui.FileUpload.EventType = {
+  UPLOAD_START: 'upload-start',
   UPLOAD_COMPLETE: 'upload-complete',
-  UPLOAD_ERROR: 'upload-error'
+  UPLOAD_ERROR: 'upload-error'  
 };
