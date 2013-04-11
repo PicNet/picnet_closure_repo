e65af1a22781c52df17bb630f3cc07455ef73706
Guido Tapia <guido@tapia.com.au>
2013-04-11 11:07:52 +1000

    Fixed Select Editor

--- src/pn/slick.grid.externs.js
+++ src/pn/slick.grid.externs.js
@@ -1120,7 +1120,7 @@ Slick.Grid.prototype.destroy = function() {};
 
 
 /**
- * @return {*}
+ * @return {{commitCurrentEdit:function():undefined}}
  */
 Slick.Grid.prototype.getEditorLock = function() {};
 
--- src/pn/ui/grid/editors/SelectEditor.js
+++ src/pn/ui/grid/editors/SelectEditor.js
@@ -5,80 +5,123 @@ goog.provide('pn.ui.grid.editors.SelectEditor');
 
 /**
  * @constructor
- * @param {{
- *    column: {
- *      options: string,
- *      field: string
- *    },
- *    container: !Element
- * }} args The arguments for this editor.
+ * @param {{grid: Slick.Grid,
+ *    column: {options: string, field: string }, container: !Element }}
+ *    args The arguments for this editor.
  */
 pn.ui.grid.editors.SelectEditor = function(args) {
   pn.assObj(args);
   pn.assObj(args['column']);
 
-  var $select,
-      defaultValue,
-      values,
-      str;
+  /**
+   * @private
+   * @type {{grid: Slick.Grid,
+   *    column: {options: string, field: string }, container: !Element }}
+   */
+  this.args_ = args;
 
-  /** @expose */
-  this.init = function() {
-    if (args['column']['options']) { values = args['column']['options'].split(','); }
-    else { values = ['yes', 'no']; }
-    str = '';
-    values.pnforEach(function(v) {
-      str += "<OPTION value='" + v + "'>" + v + '</OPTION>';
-    });
-    $select = $("<SELECT tabIndex='0' class='editor-select'>%s</SELECT>".
-        pnsubs(str));
-    $select.appendTo(args['container']);
-    $select.focus();
-    $select.change(args.grid.getEditorLock().commitCurrentEdit);
+  /**
+   * @private
+   * @type {jQuery}
+   */
+  this.ctl_;
+
+  /**
+   * @private
+   * @type {string}
+   */
+  this.defaultv_;
+
+  this.init_();
+};
+
+
+/** @private */
+pn.ui.grid.editors.SelectEditor.prototype.init_ = function() {
+  var values = ['yes', 'no'];
+  if (this.args_['column']['options']) {
+    values = this.args_['column']['options'].split(',');
+  }
+  var str = '';
+  values.pnforEach(function(v) {
+    str += "<OPTION value='" + v + "'>" + v + '</OPTION>';
+  });
+  this.ctl_ = $("<SELECT tabIndex='0' class='editor-select'>%s</SELECT>".
+      pnsubs(str));
+  this.ctl_.appendTo(this.args_['container']);
+  this.ctl_.focus();
+  this.ctl_.change(this.args_['grid'].getEditorLock().commitCurrentEdit);
+};
+
+
+/** @expose */
+pn.ui.grid.editors.SelectEditor.prototype.destroy = function() {
+  this.ctl_.remove();
+};
+
+
+/** @expose */
+pn.ui.grid.editors.SelectEditor.prototype.focus = function() {
+  this.ctl_.focus();
+};
+
+
+/**
+ * @expose
+ * @param {!Object} item The item to load.
+ */
+pn.ui.grid.editors.SelectEditor.prototype.loadValue = function(item) {
+  pn.assObj(item);
+
+  this.defaultv_ = item[this.args_['column']['field']];
+  this.ctl_.val(this.defaultv_);
+};
+
+
+/**
+ * @expose
+ * @return {string|boolean} The value of the current select control.
+ */
+pn.ui.grid.editors.SelectEditor.prototype.serializeValue = function() {
+  var val = /** @type {string} */ (this.ctl_.val());
+  if (this.args_['column']['options']) {
+    return val;
+  } else {
+    return (val == 'yes');
+  }
+};
+
+
+/**
+ * @expose
+ * @param {!Object} item The item to save the value to.
+ * @param {string} state The value selected.
+ */
+pn.ui.grid.editors.SelectEditor.prototype.applyValue = function(item, state) {
+  pn.assObj(item);
+  pn.assStr(state);
+
+  item[this.args_['column']['field']] = state;
+};
+
+
+/**
+ * @expose
+ * @return {boolean} Wether the value is dirty (has changed).
+ */
+pn.ui.grid.editors.SelectEditor.prototype.isValueChanged = function() {
+  return (this.ctl_.val() != this.defaultv_);
+};
+
+
+/**
+ * @expose
+ * @return {{valid:boolean, msg:string?}} The validation information for
+ *    this control.
+ */
+pn.ui.grid.editors.SelectEditor.prototype.validate = function() {
+  return {
+    'valid': true,
+    'msg': null
   };
-
-  /** @expose */
-  this.destroy = function() {
-    $select.remove();
-  };
-
-  /** @expose */
-  this.focus = function() {
-    $select.focus();
-  };
-
-  /** @expose */
-  this.loadValue = function(item) {
-    defaultValue = item[args['column']['field']];
-    $select.val(defaultValue);
-  };
-
-  /** @expose */
-  this.serializeValue = function() {
-    if (args['column']['options']) {
-      return $select.val();
-    } else {
-      return ($select.val() == 'yes');
-    }
-  };
-
-  /** @expose */
-  this.applyValue = function(item, state) {
-    item[args['column']['field']] = state;
-  };
-
-  /** @expose */
-  this.isValueChanged = function() {
-    return ($select.val() != defaultValue);
-  };
-
-  /** @expose */
-  this.validate = function() {
-    return {
-      valid: true,
-      msg: null
-    };
-  };
-
-  this.init();
 };
