c1a2116a8e84fc1f2e9524d19c62820cfe4f9621
Guido Tapia <guido@tapia.com.au>
2013-06-05 08:39:37 +1000

    Added some assertions to export command

--- src/pn/web/BaseWebApp.js
+++ src/pn/web/BaseWebApp.js
@@ -171,6 +171,11 @@ pn.web.BaseWebApp.prototype.acceptDirty = function() {
  * @param {Array.<Array.<string>>} data The data to export.
  */
 pn.web.BaseWebApp.prototype.listExport_ = function(type, format, data) {
+  pn.assStr(type);
+  pn.assStr(format);
+  pn.assArr(data);
+  pn.ass(format !== '0');
+
   var ed = {'exportType': format, 'exportData': pn.json.serialiseJson(data)};
   var uri = this.cfg.touri('ExportData', 'ExportData');
   pn.data.DataDownloader.send(uri, ed);
