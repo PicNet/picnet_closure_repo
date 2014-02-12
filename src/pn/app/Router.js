b91beb4156f37eda0fc76d57da2605df0eb5f641
Guido Tapia <guido@tapia.com.au>
2014-02-12 14:41:48 +1100

    Fixed double back issue

--- src/pn/app/Router.js
+++ src/pn/app/Router.js
@@ -50,6 +50,13 @@ pn.app.Router = function(opt_defaultRoute, opt_invisible) {
    */
   this.history_ = new goog.history.Html5History();
   this.registerDisposable(this.history_);
+  
+  // Hack to resolve history firing twice issues.  This is from: 
+  // https://code.google.com/p/closure-library/issues/detail?id=449
+  // goog.events.unlisten(this.history_.window_, goog.events.EventType.POPSTATE, 
+  //    this.history_.onHistoryEvent_, false, this.history_);
+  goog.events.unlisten(this.history_.window_, goog.events.EventType.HASHCHANGE, 
+      this.history_.onHistoryEvent_, false, this.history_);
 };
 goog.inherits(pn.app.Router, pn.app.EventHandlerTarget);
 
@@ -146,7 +153,7 @@ pn.app.Router.prototype.onNavigate_ = function(e) {
  * @private
  * @return {boolean} False if this navigation event is cancelled.
  */
-pn.app.Router.prototype.fireNavigating_ = function() {
+pn.app.Router.prototype.fireNavigating_ = function() {  
   var e = new goog.events.Event(pn.app.Router.EventType.NAVIGATING);
   var continuing = this.dispatchEvent(e);
   this.log_.fine('fireNavigating_ continuing: ' + continuing);
