7b1d9c2337d7a471ce4da84db5f8adc2f68a97cd
Guido Tapia <guido@tapia.com.au>
2014-02-12 11:49:27 +1100

    Create Guid simulator creator

---
+++ src/pn/guid.js
@@ -0,0 +1,16 @@
+﻿;
+goog.provide('pn.guid');
+
+
+/** @return {string} A random number with the appearance of a guid. */
+pn.guid.generate = function() {
+  var s4 = pn.guid.generate.s4_;
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+         s4() + '-' + s4() + s4() + s4();
+};
+
+
+/** @private @return {string} */
+pn.guid.generate.s4_ = function() {
+  return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+};
