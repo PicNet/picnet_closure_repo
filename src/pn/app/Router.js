7a24e63cef628a9aae6b5ba60b77fc7deef60707
Guido Tapia <guido@tapia.com.au>
2014-03-27 11:56:46 +1100

    Router now handles navigation to same page (for refreshing purposes)

--- src/pn/app/Router.js
+++ src/pn/app/Router.js
@@ -127,9 +127,12 @@ pn.app.Router.prototype.replaceLocation = function(path) {
  */
 pn.app.Router.prototype.navigate = function(path) {
   pn.ass(path);
-  this.log_.fine('path: ' + path + ' added to history stack');
+  this.log_.fine('path: ' + path + ' added to history stack');  
+  // Html5 History ignores navitation to same path however this is needed
+  //    if we want to refresh the current page so manually force the navigation.
+  if (path === this.history_.getToken()) { this.navigateImpl_(path); }
   // This will trigger a NAVIGATE event which will inturn call navigateImpl_
-  this.history_.setToken(path);
+  else { this.history_.setToken(path); }
 };
 
 
