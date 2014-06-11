1175d1574d66bbd5e4f7c4c0eb16a655885ae11e
Guido Tapia <guido@tapia.com.au>
2014-06-11 15:17:02 +1000

    Minor closure comment enhancement

--- src/pn/ui/BaseControl.js
+++ src/pn/ui/BaseControl.js
@@ -181,7 +181,7 @@ pn.ui.BaseControl.prototype.hasel = function(id) {
  * @param {string|!Element|!Array.<(string|!Element)>} el The ID or element
  *    of the element to listen to. This uses the standard naming conventions
  *    for element IDs.
- * @param {function():undefined} cb The callback for the event.
+ * @param {function(goog.events.Event):undefined} cb The callback for the event.
  */
 pn.ui.BaseControl.prototype.onchange = function(el, cb) {
   this.getels_(el).pnforEach(function(e) {
