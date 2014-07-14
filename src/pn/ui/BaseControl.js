281c4a2b942f4dd9b3a39c3d85851419c065ea89
Steven Zhang <steven.zhang@picnet.com.au>
2014-07-15 09:11:54 +1000

    add param to onkeyup

--- src/pn/ui/BaseControl.js
+++ src/pn/ui/BaseControl.js
@@ -196,7 +196,7 @@ pn.ui.BaseControl.prototype.onchange = function(el, cb) {
  * @param {string|!Element|!Array.<(string|!Element)>} el The ID or element
  *    of the element to listen to. This uses the standard naming conventions
  *    for element IDs.
- * @param {function():undefined} cb The callback for the event.
+ * @param {function(goog.events.Event):undefined} cb The callback for the event.
  */
 pn.ui.BaseControl.prototype.onkeyup = function(el, cb) {
   this.getels_(el).pnforEach(function(e) {
